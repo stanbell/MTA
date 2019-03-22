11cb159ff853dcfbbed92dae87135adc2ebd9e0f
stanbell <stan@stanbell.com>
2019-03-22 13:21:52 -0500

    filter transactions for client

--- src/pages/transactions/transactions.ts
+++ src/pages/transactions/transactions.ts
@@ -10,7 +10,8 @@ import { UserDataProvider } from '../../providers/user-data/user-data';
   templateUrl: 'transactions.html',
 })
 export class TransactionsPage {
-  filter: any;
+  mainTransPage: boolean = true;
+  filter: ClientType;
   itemsList: any;
 
   constructor(public navCtrl: NavController,
@@ -18,14 +19,18 @@ export class TransactionsPage {
     public helper: HelpersProvider,
     public trans: TransactionsProvider,
     public ud: UserDataProvider) {
-      this.filter = navParams.get('client');
+    this.filter = navParams.get('client');
+    this.mainTransPage = (!!this.filter) ? false : true; 
     trans.read();
   }
-  
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad TransactionsPage');
-    console.log('trans', this.trans.transactions);
-    this.populateItems();
+    // console.log('trans', this.trans.transactions);
+    // console.log((this.filter) ? this.filter.name : 'no filter');
+    (this.filter) ? this.populateItems(this.filter.name) : this.populateItems();
+    // console.log(this.itemsList);
+    
   }
 
   ionViewWillLeave() {
@@ -38,29 +43,36 @@ export class TransactionsPage {
 
   }
 
-  populateItems() {
-
-    // WORKING HERE:  edit to filter for one client only
-    // this.filter
+  populateItems(filter?: string) {
 
     this.itemsList = [];
     let ttl: number = 0;
-    for (let i = 0; i<this.trans.transactions.length; i++) {
+
+    for (let i = 0; i < this.trans.transactions.length; i++) {
+      const cName = this.findClientName(this.trans.transactions[i].apptId);
+      if (filter) {  // if filtering
+        if (cName !== filter) {  // skip if doesn't match
+          console.log('doesnt match ', cName);
+          continue;
+        }
+      }
+      console.log('should match ', cName);
       this.itemsList.push(this.trans.transactions[i]);
-      this.itemsList[i]['clientName'] = this.findClientName(this.itemsList[i].apptId);
-      let d = new Date(this.itemsList[i].date);
+      const j = this.itemsList.length - 1;  // last one added
+      this.itemsList[j]['clientName'] = cName;
+      let d = new Date(this.itemsList[j].date);
       let y = d.getFullYear().toString();
       y = y.substr(2, 2);
-      this.itemsList[i]['shortDate'] = (d.getMonth() + 1) + '/' + y;
-      this.itemsList[i]['formattedAmount'] = (this.itemsList[i].amount >= 0) 
-        ? this.itemsList[i].amount.toFixed(2) 
-        : "(" + (0 - this.itemsList[i].amount).toFixed(2) + ")";
-      this.itemsList[i]['reconciledIcon'] = (this.itemsList[i].reconciled) 
-        ? 'checkmark-circle' 
+      this.itemsList[j]['shortDate'] = (d.getMonth() + 1) + '/' + y;
+      this.itemsList[j]['formattedAmount'] = (this.itemsList[j].amount >= 0)
+        ? this.itemsList[j].amount.toFixed(2)
+        : "(" + (0 - this.itemsList[j].amount).toFixed(2) + ")";
+      this.itemsList[j]['reconciledIcon'] = (this.itemsList[j].reconciled)
+        ? 'checkmark-circle'
         : 'remove-circle-outline';
-      if (this.itemsList[i].amount > 0) ttl += this.itemsList[i].amount;
+      if (this.itemsList[j].amount > 0) ttl += this.itemsList[j].amount;
     }
-    this.itemsList.push({shortDate:'', description: 'Total', formattedAmount: ttl.toFixed(2), reconciledIcon: 'none'})
+    this.itemsList.push({ shortDate: '', description: 'Total', formattedAmount: ttl.toFixed(2), reconciledIcon: 'none' })
   }
 
   findClientName(apptId: string): string {
