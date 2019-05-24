cf92b09e31be03822ed13e358a6f7c5628d53737
stanbell <stan@stanbell.com>
2019-05-24 15:39:26 -0500

    clean module references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,21 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { IonicStorageModule } from '@ionic/storage';
 import { HttpClientModule } from '@angular/common/http';
 import { Calendar } from '@ionic-native/calendar';
-// import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { MyApp } from './app.component';
-// import { Auth0Provider } from '../providers/auth0/auth0';
 import { UserDataProvider } from '../providers/user-data/user-data';
 import { ClientsProvider } from '../providers/clients/clients';
 import { TransactionsProvider } from '../providers/transactions/transactions';
 import { ScheduleProvider } from '../providers/schedule/schedule';
-import { DocumentsProvider } from '../providers/documents/documents';
 import { HelpersProvider } from '../providers/helpers/helpers';
 import { MTAAPI } from '../providers/mtaapi/mtaapi';
 import { CCAPI } from '../providers/ccapi/ccapi';
 import { CacheProvider } from '../providers/cache/cache';
 import { LocalStoreProvider } from '../providers/local-store/local-store';
-// import { Stripe } from '@ionic-native/stripe/ngx';
 
 import { LoginPage } from '../pages/login/login';
 import { ClientsPage } from '../pages/clients/clients';
@@ -49,13 +46,11 @@ import { StatsPage } from '../pages/stats/stats';
 import { EditServicePage } from '../pages/edit-service/edit-service';
 import { SignupPage } from '../pages/signup/signup';
 import { PaymentPage } from '../pages/payment/payment';
-import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { BiznavPage } from '../pages/biznav/biznav';
 import { BiztodayPage } from '../pages/biztoday/biztoday';
 import { AccountPage } from '../pages/account/account';
 import { AccountProvider } from '../providers/account/account';
 import { DownloadPage } from '../pages/download/download';
-// import { File } from '@ionic-native/file';
 
 @NgModule({
   declarations: [
@@ -125,28 +120,24 @@ import { DownloadPage } from '../pages/download/download';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Storage,
+    Calendar,
+    InAppBrowser,
     UserDataProvider,
     ClientsProvider,
     TransactionsProvider,
     ScheduleProvider,
-    DocumentsProvider,
     HelpersProvider,
     MTAAPI,
     CCAPI,
     CacheProvider,
     LocalStoreProvider,
-    Storage,
-    Calendar,
-    EventPage,
-    DocumentsProvider,
-    InAppBrowser,
     EmptiesProvider,
     UserProvider,
     UserDataWriterProvider,
     AuthProvider,
     AuthapiProvider,
     AccountProvider,
-    // File,
   ]
 })
 export class AppModule {}
