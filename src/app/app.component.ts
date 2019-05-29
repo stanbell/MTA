e43ca2bdc835961296cdb869b2d969d3d6084ceb
stanbell <stan@stanbell.com>
2019-05-29 15:25:53 -0500

    improve browser behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,8 +32,10 @@ export class MyApp {
       this.platform.ready().then(() => {
         // Okay, so the platform is ready and our plugins are available.
         // Here you can do any higher level native things you might need.
-        this.statusBar.styleDefault();
-        this.splashScreen.hide();
+        if (this.platform.is('cordova')) {
+          this.statusBar.styleDefault();
+          this.splashScreen.hide();
+        }
         
         // initialize primary data store
         this.ud.initData();
